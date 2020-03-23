0093f290bb9dfebe602ffa3837cbb01c1d246f77
butch7903 <31591018+butch7903@users.noreply.github.com>
2020-03-23 10:12:43 -0500

    Update Generate Root and Intermediate Certificates From Internal CA.ps1

--- Generate Root and Intermediate Certificates From Internal CA.ps1
+++ Generate Root and Intermediate Certificates From Internal CA.ps1
@@ -21,13 +21,13 @@
 
 #Customizable Variables
 $ROOTMATCH = "hamker" #Match the name of the CAs you wish you gather certificates from. You will want to look at this via MMC>Certificates>Computer Account>Root Certificates Authorities
-$INTEREDIATEMATCH = "hamker" #Match the name of the CAs you wish you gather certificates from. You will want to look at this via MMC>Certificates>Computer Account>Intermediate Certificates Authorities
+$INTERMEDIATEMATCH = "hamker" #Match the name of the CAs you wish you gather certificates from. You will want to look at this via MMC>Certificates>Computer Account>Intermediate Certificates Authorities
 $OpenSSLLocation = "C:\Program Files\OpenSSL-Win64\bin" #x64 Version
 
 #Standard Variables
 $DATE = Get-date #Todays Date. Used to verify a CA Certicate is still valid
 $RootCerts = get-childitem -path Cert:\LocalMachine\Root | Sort Subject | Where {$_.Subject -match $ROOTMATCH -and $_.NotAfter -gt $DATE}
-$InterCerts = get-childitem -path Cert:\LocalMachine\CA | Sort Subject | Where {$_.Subject -match $INTEREDIATEMATCH -and $_.NotAfter -gt $DATE}
+$InterCerts = get-childitem -path Cert:\LocalMachine\CA | Sort Subject | Where {$_.Subject -match $INTERMEDIATEMATCH -and $_.NotAfter -gt $DATE}
 
 ###Test if OpenSSL is Installed
 ##Specify OpenSSL version. If you have a 64-bit OS, use the x64 version. If you have a 32-bit OS, use the x86 version
